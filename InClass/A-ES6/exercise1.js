1cbbdbac8e1ce81006709308a35341f69a6d9462
jorgesr92 <jorgesr92@github.com>
2020-09-29 22:11:55 +0200

    update exercise 1 of A-ES6

--- InClass/A-ES6/exercise1.js
+++ InClass/A-ES6/exercise1.js
@@ -10,7 +10,7 @@ console.log(defaultParameter());
 // Use the spread operator to combine arr1 and arr2
 
 
-const combineArrays = (arr1, arr2) => [...arr1, arr2];
+const combineArrays = (arr1, arr2) => [...arr1, ...arr2];
 
 console.log(combineArrays([1,2,3], [4]));
 

a74547d93f3ab385a5f4fc9a973431d40456c8ee
jorgesr92 <jorgesr92@github.com>
2020-10-03 13:38:21 +0200

    2020.10.03 class

--- InClass/A-ES6/exercise1.js
+++ InClass/A-ES6/exercise1.js
@@ -8,8 +8,6 @@ console.log(defaultParameter("jorge"));
 console.log(defaultParameter());
 
 // Use the spread operator to combine arr1 and arr2
-
-
 const combineArrays = (arr1, arr2) => [...arr1, ...arr2];
 
 console.log(combineArrays([1,2,3], [4]));
--- InClass/A-ES6/exercise2.js
+++ InClass/A-ES6/exercise2.js
@@ -1,23 +1,26 @@
 class Polygon {
-  constructor(height, width) {
-    this.name = "Polygon";
+  constructor(name = "Polygon", height, width) {
+    this.name = name;
     this.height = height;
     this.width = width;
   }
   sayName() {
     console.log('Hi, I am a ' + this.name);
   }
+
+  calculateArea(){
+    return this.height * this.width;
+  }
 }
 
 let p = new Polygon('Polygon', 300, 400);
 p.sayName();
-console.log('The area of this polygon is ' + p.area);
+console.log('The area of this polygon is ' + p.calculateArea());
 
 
 class Rectangle extends Polygon {
   constructor(height, width) {
-    super(height, width);
-    this.name = "Rectangle";
+    super("Rectangle", height, width);
     this.sides = 4;
     //this.area = this.height * this.width;
   }
@@ -30,7 +33,7 @@ class Rectangle extends Polygon {
 let r = new Rectangle(50, 60);
 r.sayName();
 //r.area = 3;
-console.log('The area of this polygon is ' + r.area);
+console.log('The area of this polygon is ' + r.area );
 
 //Create 2 new classes that extend from polygon: triangle and circle and log their names and areas
 
